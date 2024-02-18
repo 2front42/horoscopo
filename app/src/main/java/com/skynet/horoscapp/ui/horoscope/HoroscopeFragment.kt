e8e0ce881595ef5496380a917f13f8a58a3c88c3
max <islaremota@gmail.com>
2024-02-18 23:39:47 +0100

    last update

--- app/src/main/java/com/skynet/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/skynet/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,13 +1,20 @@
 package com.skynet.horoscapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.skynet.horoscapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 //recibir cosas injectadas
 @AndroidEntryPoint
@@ -15,8 +22,40 @@ import dagger.hilt.android.AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
 
     private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
+
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        InitList()
+        initUIState()
+    }
+
+    private fun InitList() {
+        horoscopeAdapter = HoroscopeAdapter()
+        binding.rvHoroscope.apply {
+            layoutManager = GridLayoutManager(context,2)
+            adapter = horoscopeAdapter
+        }
+
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    //cambios en horoscope
+                    horoscopeAdapter.updateList(it)
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/skynet/horoscapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/skynet/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,10 +1,29 @@
 package com.skynet.horoscapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.skynet.horoscapp.domain.model.HoroscopeInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 //recibir cosas injectadas
 @HiltViewModel
 class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value = listOf(
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini,
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini,
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini,
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini,
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini,
+            HoroscopeInfo.Aries, HoroscopeInfo.Taurus, HoroscopeInfo.Gemini
+        )
+    }
+
+
 }
