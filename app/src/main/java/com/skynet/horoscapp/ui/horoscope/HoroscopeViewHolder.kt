f00a9987ef871844c24c8a15e05a3fcef6f91966
max <islaremota@gmail.com>
2024-02-19 21:00:49 +0100

    add rotation animation

--- app/src/main/java/com/skynet/horoscapp/ui/horoscope/HoroscopeViewHolder.kt
+++ app/src/main/java/com/skynet/horoscapp/ui/horoscope/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.skynet.horoscapp.ui.horoscope
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.skynet.horoscapp.databinding.ItemHoroscopeBinding
 import com.skynet.horoscapp.domain.model.HoroscopeInfo
@@ -13,6 +14,19 @@ class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvTitle.text = context.getString(horoscopeInfo.name)
 
-        binding.parent.setOnClickListener { onItemSelected(horoscopeInfo) }
+        //cuando hemos pulsado
+        binding.parent.setOnClickListener {
+            startRotationAnimation(binding.ivHoroscope)
+            onItemSelected(horoscopeInfo)
+        }
+    }
+
+    private fun startRotationAnimation(view: View) {
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            start()
+        }
     }
 }
